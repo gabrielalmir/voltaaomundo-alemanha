5e220a8c5286653a25e1cf04e2ba1ac77688a176
gabrielalmir <gabr.almir@gmail.com>
2023-11-08 18:13:01 -0300

    feat: remove alert in admin page

--- src/components/MessageBlock/index.tsx
+++ src/components/MessageBlock/index.tsx
@@ -9,49 +9,42 @@ export default function MessageBlock({ message }: { message: ContactMessage }) {
   const [disabled, setDisabled] = useState(false);
 
   async function handler(id: string) {
-    setDisabled(true)
-    const response = await api.put(
+    setDisabled(true);
+    return await api.put(
       "contact",
       { id, active: false },
     );
-
-    if (response.status === 200) {
-      return alert("Mensagem apagada com sucesso!");
-      // return window.location.href = window.location.href;
-    }
-
-    return alert("Falha ao apagar mensagem");
   }
 
   return (
-    <>
-      <div key={message.id}>
-        <div className="message-block">
-          <div className="message">
-            <div className="message-header">
-              <h3>
-                #{message.id.substring(0, 4)} {message.name}
-              </h3>
-              <span>{message.createdAt.toLocaleString()}</span>
-            </div>
-            <div className="message-content">
-              <p>{message.message}</p>
-            </div>
+    <div key={message.id}>
+      <div className="message-block" style={{ opacity: disabled ? 0.5 : 1 }}>
+        <div className="message">
+          <div className="message-header">
+            <h3>
+              #{message.id.substring(0, 4)} {message.name}
+            </h3>
+            <span>{message.createdAt.toLocaleString()}</span>
           </div>
-          <div className="message-actions">
-            {disabled ? (
-              <button className="btn btn-sm btn-warning" disabled={true}>Processando</button>
-            ) : (
-              <button
-                className="btn btn-sm btn-danger"
-                onClick={() => handler(message.id)}
-              >
-                Apagar
-              </button>
-            )}
+          <div className="message-content">
+            <p>{message.message}</p>
           </div>
         </div>
+        <div className="message-actions">
+          {disabled ? (
+            <button className="btn btn-sm btn-warning" disabled={true}>
+              Processando
+            </button>
+          ) : (
+            <button
+              className="btn btn-sm btn-danger"
+              onClick={() => handler(message.id)}
+            >
+              Apagar
+            </button>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
